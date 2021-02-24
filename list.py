1d6ac664db8f1aa4b68003b0702ffffee61e9870
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2021-02-25 01:50:37 +0530

    Two FILES ADDED

--- list.py
+++ list.py
@@ -1,4 +1,18 @@
-a = [10 ,30.56 , "hello"]
-print(a[2])
-a[1] = 21
-print(a)
+list =[] #  empty list
+list1 = ['one' , 'two' , 'three' , 'four'] #list of strings
+
+list2 = [1,2,3,4] # list of Integer
+list3 = [[1,2] , [3,4]]  # lsit of list
+list4 = [1 , 'ramu' , 24 , 1.23] #list of different data types
+ 
+print(list)
+print(list1 , end ="")
+print(list2)
+print(list3)
+print(list4)
+
+             
+
+
+ 
+ 
---
+++ listAppend.py
@@ -0,0 +1,9 @@
+lst=['one' , 'two' , 'Three' ,' Four']
+lst2 =['five' , 'six']
+lst.append(lst2)
+print(lst)
+lst=['one' , 'two' , 'Three' ,' Four']
+lst2 =['five' , 'six']
+lst.extend(lst2)
+print(lst)
+
