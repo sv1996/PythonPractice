cc3011b2c0219b2c217bed72e10770acb0afea87
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2021-02-27 02:45:31 +0530

    String concatination and method Added

--- string.py
+++ string.py
@@ -13,6 +13,31 @@ for l in "Hello World":
     if l=='o':
         count+=1
 print(count , "Letter Found")
+
+#using simple iteration and range()
+#code1
+String = "Gekksforgeeks"
+#iterate through strings
+for element in String:
+    print(element ,  end ='')
+print("\n")
+
+#code2
+
+StringName = "GEKKSFORGEEKS"
+#Iterate over indexed
+
+for element in range(0 ,len(StringName)):
+    print(StringName[element])
+
+ #Iterarte over reverse 
+for c in StringName[::-1]:
+    print(c , end = ' ')
+    
+    for c in reversed(StringName):
+        print(c , end = ' ')
  
+   
+
  
  
